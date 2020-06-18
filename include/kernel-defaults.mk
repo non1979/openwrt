7cfb0d3eccdde1a1faa786a3958d45d5e13842dd
ALEX ART <artamonov@gmail.com>
2020-06-18 23:32:17 +0300

    Revert "use official repo for kmod 5.4.45-1-8b3aaa48ce5dfb66263bde9605e79a76"
    This reverts commit fe732e509ad4a09970854009a5168dac12d5bad7.

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -105,7 +105,7 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | echo "8b3aaa48ce5dfb66263bde9605e79a76" > $(LINUX_DIR)/.vermagic
+	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | mkhash md5 > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
